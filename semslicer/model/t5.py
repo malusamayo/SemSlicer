1317a0e9553a5fa897ef5a8870865ad049a63c5b
malusamayo <773386@gmail.com>
2024-02-02 11:51:07 -0500

    fix t5 tokenizer maxlen

--- semslicer/model/t5.py
+++ semslicer/model/t5.py
@@ -50,6 +50,7 @@ class FlanT5Wrapper:
         START_TIME = time.perf_counter()
         logger.info("Start loading {}...".format(model_name))
         self.tokenizer = T5Tokenizer.from_pretrained(model_name)
+        self.tokenizer.model_max_length=4096
         # device_map = {
         #     'model.shared': "nan",
         #     'model.encoder': "nan", 
