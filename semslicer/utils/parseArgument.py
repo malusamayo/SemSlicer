6f0e9cace5a57e9018c699b30fa2ad35f3427ebe
malusamayo <773386@gmail.com>
2024-02-20 22:50:25 -0500

    update config

--- semslicer/utils/parseArgument.py
+++ semslicer/utils/parseArgument.py
@@ -11,4 +11,5 @@ def addArg(parser):
                         help="Selected from: [slicing, find_prompts, run_model, prompt_analysis, label]")
     parser.add_argument("--verbose", choices=["DEBUG", "INFO", "WARN"], default="INFO", \
                         help="Selected from: [debug, info, warn]")
+    parser.add_argument("--config_path", type=str, default="config.yaml", help="config file path")
     parser.add_argument("--exp_name", type=str, default="exp", help="experiment name")
